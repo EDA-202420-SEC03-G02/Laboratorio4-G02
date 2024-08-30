3203fde055b6369a077332c44df1bf9b1d77558c
j.sanchezf@uniandes.edu.co <j.sanchezf@uniandes.edu.co>
2024-08-30 08:55:44 -0500

    cambio est-3

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -1,11 +1,21 @@
 from DataStructures.List import list_node as node
 def add_last(lst,elem):
     nodo= node.new_single_node(elem)
-    lst["last"]["next"]=nodo
-    lst["last"]=nodo
-    lst["size"]+=1
-    if lst["size"]==1:
+    if lst["size"]==0:
         lst["first"]=nodo
-    return lst    
-    
-    
+        lst["size"]+=1
+    else:
+        lst["last"]["next"]=nodo
+        lst["last"]=nodo
+        lst["size"]+=1
+    return lst
+ 
+ def add_first(lst, elem):   
+     if lst["size"]==0:
+        lst["first"]=nodo
+        lst["size"]+=1
+     else:
+        lst["first"]["next"]=nodo
+        lst["first"]=nodo
+        lst["size"]+=1
+     return lst
